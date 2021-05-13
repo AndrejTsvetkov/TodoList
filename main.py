c8b501b1a325af0ab05d3d63f42cfd2394499151
Andrey Tsvetkov <cvetkoandrejj@gmail.com>
2021-05-13 18:18:47 +0300

    Creating simplest window in tkinter

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+
+def main():
+    root = tk.Tk()
+    root.title('ToDo List')
+    root.geometry("500x500")
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
