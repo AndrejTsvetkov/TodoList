cb7e80b977a15cefad00c2af592e0b4a3a279ef2
Andrey Tsvetkov <cvetkoandrejj@gmail.com>
2021-06-09 23:46:36 +0300

    Improved working with database, added some functions and descriptions

--- database.py
+++ database.py
@@ -1,58 +1,66 @@
 import sqlite3
 
-conn = sqlite3.connect('todolist.db')
 
-c = conn.cursor()
-# 1 - done, 0 - not
-c.execute("""CREATE TABLE IF NOT EXISTS todolist (
+def db_connect(sqlite_file):
+    """ Make connection to an SQLite database file """
+    return sqlite3.connect(sqlite_file)
+
+
+def db_close(conn):
+    """ Close connection to the database """
+    conn.close()
+
+
+def db_init(conn):
+    """ Create table and delete yesterday's not done tasks """
+    cur = conn.cursor()
+    with conn:
+        # 1 - done, 0 - not
+        cur.execute("""CREATE TABLE IF NOT EXISTS todolist (
             task_id INTEGER PRIMARY KEY AUTOINCREMENT,
             task_name TEXT NOT NULL,
             task_status INTEGER NOT NULL,
             date TEXT NOT NULL
             )""")
-conn.commit()
-# conn.close() deal with "close" issue
-# c.execute("SELECT time('now')")
-# print(c.fetchall())
+        # If we didn't finish any tasks yesterday they will be deleted automatically the next day
+        cur.execute("DELETE FROM todolist WHERE task_status = 0 AND date < date('now', 'localtime')")
 
 
-# If we didn't finish any tasks yesterday they will be deleted automatically the next day
-def delete_not_done_tasks():
+def get_current_tasks(conn):
+    """ Get current tasks """
+    cur = conn.cursor()
+    cur.execute("SELECT task_name FROM todolist WHERE task_status = 0")
+    return cur.fetchall()
+
+
+def insert_task(conn, task_name, task_status):
+    """ Insert task with it's status """
+    cur = conn.cursor()
     with conn:
-        c.execute("DELETE FROM todolist WHERE task_status = 0 AND date < date('now', 'localtime')")
+        cur.execute("INSERT INTO todolist (task_name, task_status, date) VALUES (?, ?, date('now'))",
+                    (task_name, task_status))
 
 
-def get_current_tasks():
-    delete_not_done_tasks()
-    c.execute("SELECT task_name FROM todolist WHERE task_status = 0")
-    return c.fetchall()
-
-
-def get_done_tasks():
-    c.execute("SELECT task_name, date FROM todolist WHERE task_status = 1")
-    return c.fetchall()
-
-
-def insert_task(task_name, task_status):
+def complete_task(conn, task_name):
+    """ Mark task as complete """
+    cur = conn.cursor()
     with conn:
-        c.execute("INSERT INTO todolist (task_name, task_status, date) VALUES (?, ?, date('now'))",
-                  (task_name, task_status))
-
-
-def make_task_done(task_name):
-    with conn:
-        c.execute("UPDATE todolist SET task_status = 1 WHERE task_status = 0 AND task_name = :task_name",
-                  {'task_name': task_name})
+        cur.execute("UPDATE todolist SET task_status = 1 WHERE task_status = 0 AND task_name = :task_name",
+                    {'task_name': task_name})
 
 
 # We are deleting task in that case when we added one with wrong name or we decided we wouldn't do it today,
 # so in WHERE clause we check task_status (you can't delete task that already done) and date.
-def delete_task(task_name):
+def delete_task(conn, task_name):
+    """ Delete task"""
+    cur = conn.cursor()
     with conn:
-        c.execute("DELETE FROM todolist WHERE task_status = 0 AND date == date('now') and task_name = :task_name",
-                  {'task_name': task_name})
+        cur.execute("DELETE FROM todolist WHERE task_status = 0 AND date == date('now') and task_name = :task_name",
+                    {'task_name': task_name})
 
 
-def get_todays_completed_tasks():
-    c.execute("SELECT task_name FROM todolist WHERE task_status = 1 AND date == date('now')")
-    return c.fetchall()
+def get_todays_completed_tasks(conn):
+    """ Get today's completed tasks """
+    cur = conn.cursor()
+    cur.execute("SELECT task_name FROM todolist WHERE task_status = 1 AND date == date('now')")
+    return cur.fetchall()
--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from tkinter import filedialog as fd
 import database
 from datetime import datetime, date
 
+
 class ToDo(tk.Frame):
     week_days = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
 
-    def __init__(self, master=None):
+    def __init__(self, conn, master=None):
         super().__init__(master)
+        self.conn = conn
         self.master = master
         self.master.title("To-Do")
         self.master.minsize(width=300, height=400)
@@ -93,44 +95,50 @@ class ToDo(tk.Frame):
         self.done_btn.grid(row=5, column=0, sticky="ew")
         self.statistic_btn.grid(row=6, column=0, columnspan=2, sticky="ew")
 
+        database.db_init(self.conn)
         self.get_data_from_database()
 
     def get_data_from_database(self):
-        current_list_of_tasks = database.get_current_tasks()
+        current_list_of_tasks = database.get_current_tasks(self.conn)
         for task in current_list_of_tasks:
             self.todo_list.insert(tk.END, task[0])
 
     def add_item(self):
         if self.entry_todo.get() and not self.entry_todo.get().isspace():
             self.todo_list.insert(tk.END, self.entry_todo.get())
-            database.insert_task(self.entry_todo.get(), 0)
+            database.insert_task(self.conn, self.entry_todo.get(), 0)
             self.entry_todo.delete(0, tk.END)
 
     def delete_item(self):
-        database.delete_task(self.todo_list.get(tk.ACTIVE))
+        database.delete_task(self.conn, self.todo_list.get(tk.ACTIVE))
         self.todo_list.delete(tk.ACTIVE)
 
     def done_item(self):
-        database.make_task_done(self.todo_list.get(tk.ACTIVE))
+        database.complete_task(self.conn, self.todo_list.get(tk.ACTIVE))
         self.todo_list.delete(tk.ACTIVE)
 
-    @staticmethod
-    def get_statistic():
+    def get_statistic(self):
         path = fd.askdirectory(title="Save statistic")
         default_file_name = "statistic.txt"
 
         if path:
-            todays_completed_tasks = database.get_todays_completed_tasks()
+            todays_completed_tasks = database.get_todays_completed_tasks(self.conn)
             with open(f'{path}/{default_file_name}', 'w') as wf:
                 wf.write("I`ve done today next tasks:")
-                wf.write("\n"*2)
+                wf.write("\n" * 2)
                 for task in todays_completed_tasks:
                     wf.write(f'- {task[0]}\n')
 
+
 def main():
+    default_file_path = "todolist.db"
+    conn = database.db_connect(default_file_path)
+
     root = tk.Tk()
-    app = ToDo(master=root)
+    app = ToDo(conn, master=root)
     app.mainloop()
+
+    database.db_close(conn)
 
 
 if __name__ == '__main__':
